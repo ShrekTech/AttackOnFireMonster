c46dc5a4cfbe3b4a676c832f67847a622fa7d8ed
Elliot Hayward <kezeali@gmail.com>
2015-01-24 14:58:56 +1300

    Exploring voting with gui.

---
+++ Assets/Scripts/TestStatelyBehaviour.cs
@@ -0,0 +1,195 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using MonoBehavioural;
+
+[InitialState(typeof(TestStatelyBehaviour.Airborne))]
+public partial class TestStatelyBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    protected float fart;
+
+    [DefaultHandler("Start")]
+    void Garbald()
+    {
+        Debug.Log("Default Start");
+    }
+
+    // Default handler for boing, in attribute-free style ("Default_...")
+    public void Default_Boing()
+    {
+        Debug.Log("Boing");
+    }
+
+    void Default_OnCollisionEnter(Collision c)
+    {
+        Debug.Log("Default OnCollisionEnter");
+        this.ChangeState<Grounded>();
+    }
+
+    void Default_OnCollisionExit(Collision c)
+    {
+        Debug.Log("Default OnCollisionExit");
+        this.ChangeState<Airborne>();
+    }
+
+    void Default_OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(40, 100, 250, 600));
+        GUILayout.Label(new GUIContent("Default OnGUI. Active state: " + this.GetActiveState()));
+        if (GUILayout.Button(new GUIContent("Clear states")))
+            this.ChangeState(null);
+        if (GUILayout.Button(new GUIContent("Pop state")))
+            this.PopState();
+        GUILayout.EndArea();
+    }
+
+    [State]
+    [System.Serializable]
+    public class Airborne
+    {
+        [System.NonSerialized]
+        public TestStatelyBehaviour container;
+
+        [SerializeField]
+        float blort;
+
+        void Enter()
+        {
+            container.fart += 1;
+            blort += 1;
+        }
+
+        void Boing()
+        {
+            Debug.Log("Airborne boing");
+        }
+
+        void OnGUI()
+        {
+            GUILayout.BeginArea(new Rect(40, 100, 250, 600));
+            GUILayout.Label(new GUIContent("Airborne"));
+            if (GUILayout.Button(new GUIContent("Go to Woof")))
+                container.ChangeState<Woof>();
+            if (GUILayout.Button(new GUIContent("Push Arf")))
+                container.PushState(typeof(Arf));
+            GUILayout.EndArea();
+        }
+
+        void OnCollisionEnter(Collision collision)
+        {
+            container.ChangeState(typeof(Grounded));
+        }
+    }
+
+    [State]
+    protected class Grounded
+    {
+        public TestStatelyBehaviour container;
+
+        void Enter()
+        {
+            container.fart += 1;
+        }
+
+        void Update()
+        {
+        }
+
+        void OnGUI()
+        {
+            GUILayout.BeginArea(new Rect(40, 100, 250, 600));
+            GUILayout.Label(new GUIContent("Grounded"));
+            GUILayout.EndArea();
+        }
+
+        public bool HasTheCool()
+        {
+            return true;
+        }
+
+        void OnCollisionExit(Collision collision)
+        {
+            container.ChangeState(typeof(Airborne));
+        }
+    }
+
+    [State]
+    protected class Woof
+    {
+        void OnGUI()
+        {
+            GUILayout.BeginArea(new Rect(40, 100, 250, 600));
+            GUILayout.Label(new GUIContent("Woof"));
+            GUILayout.EndArea();
+        }
+
+        protected virtual void Enter()
+        {
+            Debug.Log("Enter Woof");
+        }
+
+        protected void Exit()
+        {
+            Debug.Log("Exit Woof");
+        }
+
+        // Suppress collision handlers
+        protected void OnCollisionEnter(Collision collision)
+        {
+        }
+        protected void OnCollisionExit(Collision collision)
+        {
+        }
+    }
+
+    [State(ReplaceBase=true)]
+    [System.Serializable]
+    protected class Arf : Woof
+    {
+        public TestStatelyBehaviour container;
+
+        [SerializeField]
+        bool gruff;
+
+        protected override void Enter()
+        {
+            base.Enter();
+            Debug.Log("Enter Arf");
+        }
+
+        void OnGUI()
+        {
+            GUILayout.BeginArea(new Rect(40, 100, 250, 600));
+            GUILayout.Label(new GUIContent("Arf"));
+            if (GUILayout.Button(new GUIContent("Push Ruff")))
+                container.PushState<Ruff>();
+            if (GUILayout.Button(new GUIContent("Pop State")))
+                container.PopState();
+            GUILayout.EndArea();
+        }
+    }
+
+    [State]
+    protected class Ruff : Woof
+    {
+        public TestStatelyBehaviour container;
+
+        protected override void Enter()
+        {
+            base.Enter();
+            Debug.Log("Enter Ruff");
+        }
+
+        void OnGUI()
+        {
+            GUILayout.BeginArea(new Rect(40, 100, 250, 600));
+            GUILayout.Label(new GUIContent("Ruff"));
+            if (GUILayout.Button(new GUIContent("Push Arf")))
+                container.PushState<Arf>();
+            if (GUILayout.Button(new GUIContent("Pop State")))
+                container.PopState();
+            GUILayout.EndArea();
+        }
+    }
+}
---
+++ Assets/Scripts/Voting.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Voting : MonoBehaviour
+{
+    public const int MaxPlayers = 4;
+    [System.NonSerialized]
+    public int[] ChosenOption = new int[MaxPlayers];
+
+    public int Progress;
+
+    public MonoBehaviour ActionHandler;
+
+    void Update()
+    {
+        int option = 0;
+        if (Input.GetKeyDown(KeyCode.Q))
+            option = 1;
+        if (Input.GetKeyDown(KeyCode.W))
+            option = 2;
+        if (Input.GetKeyDown(KeyCode.E))
+            option = 3;
+
+        if (option != 0) {
+            SendOption(option);
+        }
+    }
+
+    public int MyPlayerIndex = 0;
+    // NOTE: options start at 1 (0 is "no option selected")
+    public void SendOption(int option)
+    {
+        var networkView = GetComponent<NetworkView>();
+        networkView.RPC("Vote", RPCMode.All, MyPlayerIndex, option);
+    }
+
+    void Start()
+    {
+        MyPlayerIndex = 0;
+        ++currentPlayerCount;
+    }
+
+    int currentPlayerCount = 0;
+
+    void OnPlayerConnected(NetworkPlayer player)
+    {
+        Debug.Log("Player " + currentPlayerCount + " connected from " + player.ipAddress + ":" + player.port);
+        if (currentPlayerCount < MaxPlayers) {
+            var networkView = GetComponent<NetworkView>();
+            networkView.RPC("SetPlayerIndex", player, currentPlayerCount);
+
+            ++currentPlayerCount;
+        }
+        else {
+            Network.CloseConnection(player, true);
+        }
+    }
+
+    void OnPlayerDisconnected(NetworkPlayer player)
+    {
+        --currentPlayerCount;
+
+        var networkView = GetComponent<NetworkView>();
+        for (int i = 0; i < Network.connections.Length; ++i)
+            networkView.RPC("SetPlayerIndex", Network.connections[i], i);
+    }
+
+    [RPC]
+    void SetPlayerIndex(int value)
+    {
+        Debug.Log(value);
+        MyPlayerIndex = value;
+    }
+
+    bool AllVotesAreIn()
+    {
+        // TODO: unity-test assert currentPlayerCount < ChosenOption.Length
+        for (int playerIndex = 0; playerIndex < currentPlayerCount; ++playerIndex) {
+            if (ChosenOption[playerIndex] == 0)
+                return false;
+        }
+        return true;
+    }
+
+    [RPC]
+    void ResetVotes()
+    {
+        System.Array.Clear(ChosenOption, 0, ChosenOption.Length);
+    }
+
+    [RPC]
+    void SetProgress(int value)
+    {
+        Progress = value;
+    }
+
+    [RPC]
+    void Vote(int playerIndex, int option)
+    {
+        ChosenOption[playerIndex] = option;
+
+        if (Network.isServer) {
+            if (AllVotesAreIn()) {
+                //ResetVotes();
+                var networkView = GetComponent<NetworkView>();
+                networkView.RPC("ResetVotes", RPCMode.All);
+
+                networkView.RPC("ActOnVote", RPCMode.All, Progress);
+
+                Debug.Log("Got votes");
+            }
+        }
+    }
+
+    [RPC]
+    void ActOnVote(int option)
+    {
+
+    }
+}
