ee5a6e0cde03c339acdb5d282e4950a1afc13335
Saren Currie <saren@sarencurrie.com>
2015-01-24 17:08:41 +1300

    Added basic map position randomisation and location stub.

---
+++ Assets/Scripts/Map/Location.cs
@@ -0,0 +1,8 @@
+using Unity;
+
+namespace MapScenario {
+	class Location {
+		public int GetHeight() { return 0; }
+		public int GetWidth() { return 0; }
+	}
+}
--- Assets/Scripts/Map/MapGenerator.cs
+++ Assets/Scripts/Map/MapGenerator.cs
@@ -1,9 +1,21 @@
+using Unity;
+
 namespace MapScenario {
 	public class MapGenerator {
-		public Map GenerateMap() {
-			//Place sprites for locations on map in random positions?
+		public Map GenerateMap(List<Loctaions> locations) {
+			Map map = new Map();
 			
-			return new Map();
+			//Place sprites for locations on map in random positions. TODO: This doesn't account for collisions.
+			for (Location l in locations) {
+				int x, y;
+				
+				x = Random.Range(0, screen.width - location.GetWidth());
+				y = Random.range(0, screen.height - location.GetHeight());
+				
+				map.addLocation(l, x, y);
+			}
+			
+			return map;
 		}
 	}
 }
