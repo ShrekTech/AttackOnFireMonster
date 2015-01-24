b3bffec027553ac00bf06863a851d31f49e5b672
Saren Currie <saren@sarencurrie.com>
2015-01-24 18:06:05 +1300

    Fleshed out Location class.

--- Assets/Scripts/Map/Location.cs
+++ Assets/Scripts/Map/Location.cs
@@ -2,7 +2,16 @@ using Unity;
 
 namespace MapScenario {
 	class Location {
-		public int GetHeight() { return 0; }
-		public int GetWidth() { return 0; }
+		private string name;
+		private Sprite sprite;
+		//TODO: Maybe add enemies specific to each location?
+		
+		public Location(string name, Sprite sprite) {
+			this.name = name;
+			this.sprite = sprite;
+		}
+		
+		public int GetHeight() { return this.sprtie.rect.height; }
+		public int GetWidth() { return this.sprite.rect.width; }
 	}
 }
